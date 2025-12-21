a207345100988c7af1ee445def810bb8355aa040
martinezrafael <rafaelmartinez.contato@gmail.com>
2025-12-21 20:59:39 -0300

    Exercicio: Controle de acesso com manutenção

---
+++ condicionais/controle-acesso-manutencao.js
@@ -0,0 +1,24 @@
+/*
+  Imagine que você está desenvolvendo um site de compras. A pessoa só pode finalizar a compra se o carrinho
+  não estiver vazio.
+
+  Crie um programa que, com base na variável `carrinhoVazio (booleana)`, verifique se a compra pode ser finalizada.
+  Se o carrinho não estiver vazio, mostre "Compra finalizada com sucesso!". Caso contrário, exiba: "Não é possível finalizar a compra: carrinho vazio"
+
+  Exemplo de entrada:
+  `const carrinhoVazio = false`
+
+  Saída esperada:
+  `Compra finalizada com sucesso!`
+
+  Se carrinhoVazio for true:
+  `Não é possível finalizar a compra: carrinho vazio`
+*/
+
+const carrinhoVazio = true;
+
+if (!carrinhoVazio) {
+  console.log("Compra finalizada com sucesso");
+} else {
+  console.log("Não é possível finalizar a compra: carrinho vazio");
+}
